38e745508253d149df0652ef62bbcf24d28ab6c8
Shohruxpro <shohruxpro@gmail.com>
2020-03-20 06:41:36 +0500

    fix: solution of JS Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // function check(str, bracketsConfig) {
+    let bool = true;
+    while (str.length != 0 && bool == true) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let test = bracketsConfig[i].join('');
+            if (str.includes(test)) {
+                bool = true;
+                str = str.replace(test, '');
+                break;
+            } else
+                bool = false;
+        }
+    }
+    return bool;
 }
